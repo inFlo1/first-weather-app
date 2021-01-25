705f6341c9054ac696728b7abaedcf07ec5f7424
inFlo1 <inFlo1@users.noreply.github.com>
2021-01-25 03:44:43 -0600

    Added a wrorking search engine

--- src/index.js
+++ src/index.js
@@ -56,8 +56,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function search(city) {
 let apiKey = "1625e5f34c173c9d54e03c86cc8cbf1a";
-let city = "Chicago";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("Chicago");
